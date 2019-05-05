ef080c539f2d2325cbede37a73bc0bb79fcb8cab
khancode <khancodegt@gmail.com>
2019-05-04 22:02:27 -0700

    recursively create directories if they don't exist when writing result .py files to file system

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const fs = require('fs');
+const mkdirp = require('mkdirp');
+const path = require('path');
 
-// const FILE = 'test/index.py';
 const TYPE = 'utf8';
 const REGEX = new RegExp('[\\-]*[a-zA-z]+[a-zA-Z0-9]*\\+\\+', 'g');
-
-// let text = fs.readFileSync(FILE, TYPE);
 
 const pythonFiles = [];
 
@@ -14,7 +13,13 @@ console.log('pythonFiles:', pythonFiles);
 
 pythonFiles.forEach((filePath) => {
     const resultText = parseFile(filePath, TYPE);
-    fs.writeFileSync(`build/${filePath}`, resultText, TYPE);
+    const resultFilePath = `build/${filePath}`;
+    const dirname = path.dirname(resultFilePath);
+
+    // Check if dirname path exists, if it doesn't then create it
+    mkdirp.sync(dirname);
+
+    fs.writeFileSync(resultFilePath, resultText, TYPE);
 });
 
 function recursivelyReadFiles(dir, files, fileType) {
@@ -36,19 +41,14 @@ function parseFile(filePath, type) {
 
     let match;
     while ((match = REGEX.exec(text)) != null) {
-        // console.log('match:', match);
         const textFound = match[0];
         const index = match.index;
-        // console.log('textFound:', textFound);
-        // console.log('index:', index);
 
         const varText = textFound.substring(0, textFound.length - 2);
         const pythonIncrementText = `${varText} += 1`;
 
         // Replace ++ with += 1 in original text
         const modified = text.substring(0, index) + pythonIncrementText + text.substring(index + textFound.length);
-        // console.log('modified:', modified);
-
         text = modified;
     }
 
